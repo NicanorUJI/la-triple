2362ef92ee656a5f69fe6abeff1b6cbae810fc4a
JoanBenjumeaPiles <145030236+JoanBenjumeaPiles@users.noreply.github.com>
2025-12-03 19:20:58 +0100

    fondos y parte pacman
    falta finalizar bien el pacman

---
+++ La carrasqueta del tiempo/Assets/Script/Paco-man/textpoints.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class ScoreUI : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+    public PlayerScore2D playerScore;
+
+    void Update()
+    {
+        if (playerScore.score < 10)
+        {
+            scoreText.text = "000" + playerScore.score;
+        }
+        else if(playerScore.score < 100)
+        {
+            scoreText.text = "00" + playerScore.score;
+        }
+        else if (playerScore.score < 1000)
+        {
+            scoreText.text = "0" + playerScore.score;
+        }
+        else
+        {
+            scoreText.text = "" + playerScore.score;
+        }
+    }
+}
