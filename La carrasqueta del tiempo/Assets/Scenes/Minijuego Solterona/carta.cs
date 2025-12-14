f7f9ad443a13566c8df44c22f1a9b6ad0d77820a
Marpalliser120623 <145030191+Marpalliser120623@users.noreply.github.com>
2025-12-14 20:57:21 +0100

    Solterona acabado

--- La carrasqueta del tiempo/Assets/Scenes/Minijuego Solterona/carta.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "carta", menuName = "Scriptable Objects/carta")]
-public class carta : ScriptableObject
-{
-    public Sprite carta_imagen;
-    public int tipo; //El 1 es el AS y el 2 es el Joker
-    public bool activo;
-}
